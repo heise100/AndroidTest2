d99c600ee84638b80c2afb371b9f2441cf7e57d3
heise100 <823900042@qq.com>
2019-09-18 11:51:42 +0800

    提了算了

---
+++ app/src/main/java/com/example/myapplication/ui/home/CheckActivity.java
@@ -0,0 +1,15 @@
+package com.example.myapplication.ui.home;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.myapplication.R;
+import android.os.Bundle;
+
+public class CheckActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_check);
+    }
+}
--- app/src/main/java/com/example/myapplication/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/myapplication/ui/home/HomeFragment.java
@@ -1,10 +1,13 @@
 package com.example.myapplication.ui.home;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
@@ -13,10 +16,12 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.myapplication.R;
+import com.example.myapplication.util.ToastUtil;
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
+    private Button btn_1;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -28,6 +33,25 @@ public class HomeFragment extends Fragment {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
+            }
+        });
+
+        btn_1=root.findViewById(R.id.btn_1);
+        btn_1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               // Toast.makeText(getContext(),"哈哈哈,出来了",Toast.LENGTH_SHORT).show();
+                ToastUtil.showMsg(getContext(),"哈哈,高兴不");
+            }
+        });
+
+        Button btn2=root.findViewById(R.id.btn_2);
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getActivity(),CheckActivity.class);
+                startActivity(intent);
+
             }
         });
         return root;
